36bcf07e47c81bb3abf170024c4dc4de1d69429a
IogaMaster <iogamastercode@gmail.com>
2022-05-14 10:58:01 -0600

    refactor: :recycle: Make some changes to getting the project name

--- getConfig.js
+++ getConfig.js
@@ -1,17 +1,22 @@
 const inquirer = require('inquirer');
 
 module.exports = async function getConfig(input, flags, SETTINGS) {
-	if (input[0] != null) {
-		SETTINGS.projectName = input[0];
-	} else {
-		SETTINGS.projectName = await inquirer.prompt({
-			name: 'projectName',
-			type: 'input',
-			message: 'What is the name of your project: ',
-			default() {
-				return SETTINGS.projectName;
-			}
-		}).projectName;
+	async function projectName() {
+		const projectName = 'svelte3-app';
+		if (input[0] != null) {
+			projectName = input[0];
+		} else {
+			projectName = await inquirer.prompt({
+				name: 'projectName',
+				type: 'input',
+				message: 'What is the name of your project: ',
+				default() {
+					return SETTINGS.projectName;
+				}
+			}).projectName;
+		}
+		return projectName;
 	}
+	SETTINGS.projectName = await projectName();
 	return;
 };
